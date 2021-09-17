cf08bb94007a266300f8039dd8b9c6d0e2a48f9d
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-09-17 13:41:47 +0100

    Add a router to navigate to the individual functions.

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const cors = require('cors');
 
 const registerController = require('../controllers/register');
 const signinController = require('../controllers/signin');
 const screenshotsController = require('../controllers/screenshots');
 
 require('dotenv').config();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 router.post('/register', registerController.handleRegister);
 router.post('/signin', signinController.handleSignIn);
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const routes = require('./routes/app');
+const cors = require('cors');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/', routes);
 
