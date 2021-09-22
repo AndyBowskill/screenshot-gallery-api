dda42248029632bd4c3a585c370a3c7812e4b39a
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-09-22 12:48:17 +0100

    Spilt-up the controllers and the database code.

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,4 +1,5 @@
-const db = require('../config/db');
+const database = require('../database/database');
+const db = require('../config/database');
 const bcrypt = require('bcrypt');
 
 const handleSignIn = (req, res) => {
@@ -8,46 +9,13 @@ const handleSignIn = (req, res) => {
     res.status(400).json('Error signing in an user.');
   }
 
-  db.select('email', 'hash')
-    .from('login')
-    .where('email', '=', email)
-    .then((loginEmail) => {
-      bcrypt.compare(password, loginEmail[0].hash, function (error, isValid) {
-        if (error) {
-          res.status(400).json('Error signing in an user.');
-        } else {
-          if (isValid) {
-            return db
-              .select('*')
-              .from('users')
-              .where('email', '=', email)
-              .then((user) => {
-                db.select('*')
-                  .from('screenshots')
-                  .where('email', '=', email)
-                  .orderBy('id', 'desc')
-                  .then((screenshots) => {
-                    const data = {
-                      screenshots: screenshots,
-                      user: {
-                        id: user[0].id,
-                        email: user[0].email,
-                        name: user[0].name,
-                      },
-                    };
+  const valid = database.signIn(email, password);
 
-                    res.status(200).json(data);
-                  })
-                  .catch((error) =>
-                    res.status(400).json('Error signing in an user.')
-                  );
-              });
-          } else {
-            res.status(400).json('Error signing in an user.');
-          }
-        }
-      });
-    });
+  if (valid) {
+    res.status(200).json(data);
+  } else {
+    res.status(400).json('Error signing in an user.');
+  }
 };
 
 module.exports = {
